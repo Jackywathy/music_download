938e97fc6c11c4c5fb2b881575a03959415788d0
Jack Jiang <jackywathy24@gmail.com>
2016-05-31 22:49:31 +1000

    finished?

--- README.md
+++
@@ -1,4 +0,0 @@
-# music_download
-basic file
-
-I lost all my code for this project when my computer died, so lets start again!
--- classes.py
+++ classes.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup as bs
-import urllib.request as urllib
+import urllib.request as url3
 import requests
-import urllib
-import re
-audiourl = re.compile(r'audiourl=\".+?')
 __author__ = "Jack"
+
+
 url = "http://mp.weixin.qq.com/s?__biz=MzA4MzY5Mzk1NA==&mid=2656507187&idx=5&sn=e79df6770c8f42adf738504ac84fbb1a&scene=0#wechat_redirect"
+url = "http://mp.weixin.qq.com/s?__biz=MjM5NDQ0MzIxNw==&mid=2655053804&idx=1&sn=cd76a9891ae482198556909aa2bbaa05&scene=0#rd"
 site = requests.get(url)
 soup = bs(site.text, "html.parser")
 tag2 = soup.findAll("qqmusic")
 for tag in tag2:
-    tag
+    #req = url3.Request(tag['audiourl'], headers={'User-Agent':"Firefox/2.0.0.11"})
+    print(tag['music_name'])
+    
+    url3.urlretrieve(tag['audiourl'],tag['music_name']+'.m4a')

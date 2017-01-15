763652632a51d9929c7c11a593f4e38c6be51a84
Jack Jiang <jackywathy24@gmail.com>
2017-01-15 19:37:29 +1100

    Update classes.py

--- classes.py
+++ classes.py
@@ -3,10 +3,12 @@ import urllib.request as url3
 import requests
 import os
 import re
+import time
+import datetime
 __author__ = "Jack"
-url2 = "http://mp.weixin.qq.com/s?__biz=MzIzNzAxMDI1Nw==&mid=2655445873&idx=1&sn=c938d1829e1d8b7e84818fcebd6a4c6d&scene=0#rd"
-url1 = "http://mp.weixin.qq.com/s?__biz=MzIzNzAxMDI1Nw==&mid=2655445807&idx=2&sn=722ea56916e8a597dc54a41695e92c91&scene=0#rd"
-
+#url2 = "http://mp.weixin.qq.com/s?__biz=MzIzNzAxMDI1Nw==&mid=2655445873&idx=1&sn=c938d1829e1d8b7e84818fcebd6a4c6d&scene=0#rd"
+#url1 = "http://mp.weixin.qq.com/s?__biz=MzIzNzAxMDI1Nw==&mid=2655445807&idx=2&sn=722ea56916e8a597dc54a41695e92c91&scene=0#rd"
+url1 = "http://mp.weixin.qq.com/s/ovS_vHlazFe3ov1jK0zl3A"
 
 
 def download_qq(qq_link):
@@ -16,15 +18,20 @@ def download_qq(qq_link):
     tag2 = soup.findAll("qqmusic")
     errors = []
     path = assign_folder()
+    
 
 
     for tag in tag2:
-
-        req = url3.Request(tag['audiourl'], headers={'User-Agent':"Firefox/2.0.0.11"})
+        now = datetime.datetime.now()
+        req = url3.Request(tag['audiourl'], headers={'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"})
         file_name = tag['music_name']
+        print(file_name + '.m4a')
+        if os.path.isfile(os.path.join(path, file_name + '.m4a')):
+            print(file_name, 'already found')
+            continue
         try:
             u = url3.urlopen(req, timeout=200)
-            with open(path + '\\' + file_name + '.m4a', 'wb') as f:
+            with open(os.path.join(path, file_name + '.m4a'), 'wb') as f:
                 meta = u.info()
                 print(str(meta).replace('\n','|'))
                 file_size = int(meta["Content-Length"])
@@ -44,8 +51,12 @@ def download_qq(qq_link):
             errors.append((file_name, tag['audiourl']))
             print("ERROR TIMEOUT", file_name,tag['audiourl'])
             continue
+        
+        print('time taken:', (datetime.datetime.now()-now).seconds)
+        time.sleep(1)
 
     print("DONE")
+    
     with open('error%s.txt' % path,'w',encoding='UTF-8') as w:
         for i in errors:
             print(i[0], i[1], file=w)
@@ -62,6 +73,8 @@ def _download_link(broken_link):
 
 def assign_folder():
     path = 1
+    #TODO REMOVE!
+    return str(path)
     while os.path.exists(str(path)):
         path += 1
     path = str(path)
@@ -117,8 +130,8 @@ def download_9ku(link, skips=0):
 
 
 
-download_9ku('http://m.9ku.com/laoge/500shou.htm',11)
-
+#download_9ku('http://m.9ku.com/laoge/500shou.htm',11)
+download_qq(url1)
 
 
     #download_qq('http://mp.weixin.qq.com/s?__biz=MjM5NTQ2ODgzMg==&mid=405099049&idx=1&sn=4291547f9803d4877ed28cad0e37a131&scene=0#rd')
